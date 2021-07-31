44efdeb23d7cb821e25584486d9db773e3b1828e
RVladislav <rozbash@sfedu.ru>
2021-07-31 19:14:49 +0300

    version 0.0.4 send password

--- app/src/main/java/com/example/itcity/EkranVos.java
+++ app/src/main/java/com/example/itcity/EkranVos.java
@@ -9,50 +9,54 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class EkranVos extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    EditText email;
+
+    EditText userEmail;
     Button btnvost;
+    FirebaseAuth firebaseAuth;
 
-    public void sendPasswordReset() {
-        // [START send_password_reset]
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        String emailAddress = "user@example.com";
-
-        auth.sendPasswordResetEmail(emailAddress)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "Email sent.");
-                        }
-                    }
-                });
-        // [END send_password_reset]
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ekran_vosstanovleniya);
-        email = (EditText) findViewById(R.id.email);
+
+        userEmail = (EditText) findViewById(R.id.email);
         btnvost = (Button) findViewById(R.id.buttonVosstanov);
+
+        firebaseAuth = FirebaseAuth.getInstance();
         //обработка клавиш кнопок
         View.OnClickListener onClickListener = new View.OnClickListener() {
-
 
 
             @Override
             public void onClick(View view) {
                 switch (view.getId()) {
                     case R.id.buttonVosstanov:
-                        sendPasswordReset();
-                        Intent intent = new Intent(EkranVos.this, ActivityAuthor.class);
-                        startActivity(intent);
+                        firebaseAuth.sendPasswordResetEmail(userEmail.getText().toString())
+                                .addOnCompleteListener(new OnCompleteListener<Void>()
+                                {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task)
+                                    {
+                                        if (task.isSuccessful())
+                                        {
+                                            Toast.makeText(EkranVos.this,
+                                                    "письмо для сброса пароля отправлено на ваш электронный адрес", Toast.LENGTH_LONG).show();
+                                        }
+                                        else
+                                            {
+                                                Toast.makeText(EkranVos.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                                            }
+                                    }
+                                });
+
                         break;
 
                     default:
