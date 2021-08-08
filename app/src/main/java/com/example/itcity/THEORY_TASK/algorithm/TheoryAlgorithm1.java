9fe7d561ef875416e0ee889e3a1123498cee039c
RVladislav <rozbash@sfedu.ru>
2021-08-08 18:17:50 +0300

    version 0.1.1

--- app/src/main/java/com/example/itcity/THEORY_TASK/algorithm/TheoryAlgorithm1.java
+++ app/src/main/java/com/example/itcity/THEORY_TASK/algorithm/TheoryAlgorithm1.java
@@ -2,11 +2,16 @@ package com.example.itcity.THEORY_TASK.algorithm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.itcity.R;
 
 public class TheoryAlgorithm1 extends AppCompatActivity {
+    Button button8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,21 @@ public class TheoryAlgorithm1 extends AppCompatActivity {
 
         setContentView(R.layout.activity_theory_algorithm1);
         getSupportActionBar().hide();
+        button8 = (Button) findViewById(R.id.button8);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button8:
+                        Intent intent1 = new Intent(TheoryAlgorithm1.this, ZadanieAlgorithm1.class);
+                        startActivity(intent1);
+                        break;
+
+
+                }
+            }
+        };
+        button8.setOnClickListener(onClickListener);
     }
 }
