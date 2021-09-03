baa907d2fbf0d5370cba6e90699e7c27d838157c
Bovaev Arslan <arslan.bovaeff@gmail.com>
2021-09-03 15:15:01 +0300

    button and teory comp device version 0.0.0

--- app/src/main/java/com/example/itcity/THEORY_TASK/computer_Device/Computer_Teory_Task.java
+++ app/src/main/java/com/example/itcity/THEORY_TASK/computer_Device/Computer_Teory_Task.java
@@ -1,16 +1,78 @@
 package com.example.itcity.THEORY_TASK.computer_Device;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.itcity.R;
 
 public class Computer_Teory_Task extends AppCompatActivity {
+    ImageButton COMPBUTTON1;
+    ImageButton COMPBUTTON2;
+    ImageButton COMPBUTTON3;
+    ImageButton COMPBUTTON4;
+    ImageButton COMPBUTTON5;
+    ImageButton COMPBUTTON6;
+    ImageButton COMPBUTTON7;
+    ImageButton COMPBUTTON8;
+    ImageButton COMPBUTTON9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_computer__teory__task);
+
+        COMPBUTTON1 = (ImageButton) findViewById(R.id.COMPBUTTON1);
+        COMPBUTTON2 = (ImageButton) findViewById(R.id.COMPBUTTON2);
+        COMPBUTTON3 = (ImageButton) findViewById(R.id.COMPBUTTON3);
+        COMPBUTTON4 = (ImageButton) findViewById(R.id.COMPBUTTON4);
+        COMPBUTTON5 = (ImageButton) findViewById(R.id.COMPBUTTON5);
+        COMPBUTTON6 = (ImageButton) findViewById(R.id.COMPBUTTON6);
+        COMPBUTTON7 = (ImageButton) findViewById(R.id.COMPBUTTON7);
+        COMPBUTTON8 = (ImageButton) findViewById(R.id.COMPBUTTON8);
+        COMPBUTTON9 = (ImageButton) findViewById(R.id.COMPBUTTON9);
+
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.COMPBUTTON1:
+                        Intent intent1 = new Intent(Computer_Teory_Task.this, TheoryComputer1.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.COMPBUTTON2:
+                        Intent intent2 = new Intent(Computer_Teory_Task.this, TheoryComputer2.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.COMPBUTTON3:
+                        Intent intent3 = new Intent(Computer_Teory_Task.this, TheoryComputer3.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.COMPBUTTON4:
+                        Intent intent4 = new Intent(Computer_Teory_Task.this, TheoryComputer4.class);
+                        startActivity(intent4);
+                        break;
+                    case R.id.COMPBUTTON5:
+                        Intent intent5 = new Intent(Computer_Teory_Task.this, TheoryComputer5.class);
+                        startActivity(intent5);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        };
+
+        COMPBUTTON1.setOnClickListener(onClickListener);
+        COMPBUTTON2.setOnClickListener(onClickListener);
+        COMPBUTTON3.setOnClickListener(onClickListener);
+        COMPBUTTON4.setOnClickListener(onClickListener);
+        COMPBUTTON5.setOnClickListener(onClickListener);
+
+
     }
+
+
 }
