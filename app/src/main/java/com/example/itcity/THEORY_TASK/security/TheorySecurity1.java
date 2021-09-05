e1e409adc4033f6ceab0b3b27f3c93dbd5ceba1f
RVladislav <rozbash@sfedu.ru>
2021-09-05 17:58:32 +0300

    version 0.2.3 seclvl<3

--- app/src/main/java/com/example/itcity/THEORY_TASK/security/TheorySecurity1.java
+++ app/src/main/java/com/example/itcity/THEORY_TASK/security/TheorySecurity1.java
@@ -2,15 +2,34 @@ package com.example.itcity.THEORY_TASK.security;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.itcity.R;
+import com.example.itcity.THEORY_TASK.algorithm.ZadanieAlgorithm3;
 
 public class TheorySecurity1 extends AppCompatActivity {
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sectheory1);
+        next=findViewById(R.id.continueSec);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.continueSec:
+                        Intent intent = new Intent(TheorySecurity1.this,ZadanieSecurity1.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        };
+        next.setOnClickListener(onClickListener);
     }
 }
--- app/src/main/java/com/example/itcity/THEORY_TASK/security/TheorySecurity3.java
+++ app/src/main/java/com/example/itcity/THEORY_TASK/security/TheorySecurity3.java
@@ -2,15 +2,32 @@ package com.example.itcity.THEORY_TASK.security;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.itcity.R;
 
 public class TheorySecurity3 extends AppCompatActivity {
-
+Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sectheory3);
+        next = findViewById(R.id.continieSec3);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.continieSec3:
+                        Intent intent = new Intent(TheorySecurity3.this, ZadanieSecurity3.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        };
+        next.setOnClickListener(onClickListener);
     }
 }
