adbc4d88fe187e6fd1cd5011d78d7acbccad5af2
Bovaev Arslan <arslan.bovaeff@gmail.com>
2021-09-06 20:14:47 +0300

    8 ekranov inform theory version 0.0.0

---
+++ app/src/main/java/com/example/itcity/THEORY_TASK/fundamental_of_informatics/TheoryInformatica7.java
@@ -0,0 +1,31 @@
+package com.example.itcity.THEORY_TASK.fundamental_of_informatics;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.itcity.R;
+
+public class TheoryInformatica7 extends AppCompatActivity {
+    Button button8;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inftheory7);
+        button8=findViewById(R.id.button8);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button8:
+                        Intent intent = new Intent(TheoryInformatica7.this,ZadanieInformatica7.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        };
+        button8.setOnClickListener(onClickListener);
+    }
+}
