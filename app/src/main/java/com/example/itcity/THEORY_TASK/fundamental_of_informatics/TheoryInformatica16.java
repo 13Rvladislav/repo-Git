0fec6a7c1228cec1dedb35047d44bc1c0dba1385
Bovaev Arslan <arslan.bovaeff@gmail.com>
2021-09-07 14:23:43 +0300

    theory informatica version 0.0.1

---
+++ app/src/main/java/com/example/itcity/THEORY_TASK/fundamental_of_informatics/TheoryInformatica16.java
@@ -0,0 +1,32 @@
+package com.example.itcity.THEORY_TASK.fundamental_of_informatics;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.itcity.R;
+
+public class TheoryInformatica16 extends AppCompatActivity {
+    Button button8;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inftheory16);
+        button8=findViewById(R.id.button8);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button8:
+                        Intent intent = new Intent(TheoryInformatica16.this,ZadanieInformatica16.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        };
+        button8.setOnClickListener(onClickListener);
+    }
+}
