3c910757bcafbea16fc91749f4c8e38ffa8341b8
Bovaev Arslan <arslan.bovaeff@gmail.com>
2021-09-15 18:10:25 +0300

    version 0.3.4

--- app/src/main/java/com/example/itcity/THEORY_TASK/programming/Zadprog7.java
+++ app/src/main/java/com/example/itcity/THEORY_TASK/programming/Zadprog7.java
@@ -1,36 +1,38 @@
 package com.example.itcity.THEORY_TASK.programming;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.itcity.ActivityMap;
 import com.example.itcity.R;
 
-public class Thprog1 extends AppCompatActivity {
-
-    Button continueSec;
-
+public class Zadprog7 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.progtheory1);
+        setContentView(R.layout.progtask7);
         getSupportActionBar().hide();
-        continueSec = (Button) findViewById(R.id.continueSec);
+
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 switch (view.getId()) {
-                    case R.id.continueSec:
-                        Intent intent1 = new Intent(Thprog1.this, Zadprog1.class);
+                    case R.id.button5:
+                        Intent intent1 = new Intent(Zadprog7.this, Thprog7.class);
                         startActivity(intent1);
+                        break;
+                    default:
                         break;
                 }
             }
         };
-        continueSec.setOnClickListener(onClickListener);
 
     }
 }
