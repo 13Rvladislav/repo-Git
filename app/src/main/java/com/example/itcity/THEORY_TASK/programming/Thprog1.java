7c7f282ba0686afdc5f7cc559d7324d88daaee58
RVladislav <rozbash@sfedu.ru>
2021-09-15 16:31:20 +0300

    version 0.3.3

---
+++ app/src/main/java/com/example/itcity/THEORY_TASK/programming/Thprog1.java
@@ -0,0 +1,16 @@
+package com.example.itcity.THEORY_TASK.programming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itcity.R;
+
+public class Thprog1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.programming_theory1);
+    }
+}
