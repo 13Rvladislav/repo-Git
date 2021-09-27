bcdabf10b6dd18c914d3475c8aa4dc5a3d58bbc1
RVladislav <rozbash@sfedu.ru>
2021-09-27 11:20:26 +0300

    version 0.5.1

--- app/src/main/java/com/example/itcity/THEORY_TASK/computer_Device/Zadcomp6.java
+++ app/src/main/java/com/example/itcity/THEORY_TASK/computer_Device/Zadcomp6.java
@@ -1,25 +1,58 @@
 package com.example.itcity.THEORY_TASK.computer_Device;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.itcity.R;
+import com.example.itcity.THEORY_TASK.security.Security_HOME;
+import com.example.itcity.THEORY_TASK.security.TheorySecurity8;
+
+import java.util.ArrayList;
 
 public class Zadcomp6 extends AppCompatActivity {
-    Button back;
-    Button next;
+    ArrayList<PuzzlePiece> pieces;
+    int res;
+    Button button5;
+    String strres;
+    Dialog dialog;
+    Button Check;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.comptask6);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//выключение поворота экрана
         getSupportActionBar().hide();
-        back = (Button) findViewById(R.id.bottomCompDevK1);
+        button5 = findViewById(R.id.bottomCompDevK1);
+        Check = findViewById(R.id.continieCompDev);
+        final RelativeLayout layout = findViewById(R.id.Clayout);
+        pieces = splitImage();
+        TouchListener touchListener = new TouchListener();
+        for (PuzzlePiece piece : pieces) {
+            piece.setOnTouchListener(touchListener);
+            layout.addView(piece);
+        }
+
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,9 +61,137 @@ public class Zadcomp6 extends AppCompatActivity {
                         Intent intent = new Intent(Zadcomp6.this, TheoryComputer6.class);
                         startActivity(intent);
                         break;
+                    case R.id.continieCompDev:
+                        for (int i = 0; i < pieces.size(); i++) {
+                            if (pieces.get(i).canMove == false ) {
+                                res += 11;
+                            }
+                        }
+                        if (res == 99) {res += 1;}
+                        dialog = new Dialog(Zadcomp6.this);
+                        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);//скрыть заголовок
+                        dialog.setContentView(R.layout.markgooddialogwindow);//путь к макету диалогового окна
+                        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//прозрачный фон
+                        dialog.setCancelable(false);//не закрывается кнопкой назад
+                        //кнопки начало
+
+                        //кнопки конец
+                        TextView result = dialog.findViewById(R.id.mark_for_the_lvl);
+                        strres = Integer.toString(res);
+                        result.setText(strres);
+                        dialog.show();//показ окна
+
+                        Button back_to_houses= dialog.findViewById(R.id.button10);
+                        back_to_houses.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(Zadcomp6.this, Comp_Home.class);
+                                startActivity(intent);
+                            }
+                        });
+                        //ЕСЛИ БЫЛ ЗАПУЩЕН ЭТОТ БЛОК КОДА МЕНЯЕМ КОЛИЧЕСТВО БАЛЛОВ В FIREBASE  И В ПЕРЕМЕННУЮ В КОТОРОЙ НАШ УРОВЕНЬ ДЕЛАЕМ +1;
+                        break;
                 }
             }
         };
-        back.setOnClickListener(onClickListener);
+        button5.setOnClickListener(onClickListener);
+        Check.setOnClickListener(onClickListener);
+    }
+
+    private ArrayList<PuzzlePiece> splitImage() {
+        int piecesNumber = 9;
+        int rows = 3;
+        int cols = 3;
+
+        ImageView imageView = findViewById(R.id.imageView);
+        ArrayList<PuzzlePiece> pieces = new ArrayList<>(piecesNumber);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pkpuzzle1);
+
+        int pieceWidth = bitmap.getWidth()/cols;
+        int pieceHeight = bitmap.getHeight()/rows;
+
+        int yCoord = 0;
+        for (int row = 0; row < rows; row++) {
+            int xCoord = 0;
+            for (int col = 0; col < cols; col++) {
+                Bitmap pieceBitmap = Bitmap.createBitmap(bitmap, xCoord, yCoord, pieceWidth, pieceHeight);
+                PuzzlePiece piece = new PuzzlePiece(getApplicationContext());
+                piece.setImageBitmap(pieceBitmap);
+                piece.xCoord = xCoord;
+                piece.yCoord = yCoord;
+                piece.pieceWidth = pieceWidth;
+                piece.pieceHeight = pieceHeight;
+                pieces.add(piece);
+                xCoord += pieceWidth;
+            }
+            yCoord += pieceHeight;
+        }
+
+        return pieces;
+    }
+
+    public class TouchListener implements View.OnTouchListener {
+        private float xDelta;
+        private float yDelta;
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            float x = motionEvent.getRawX();
+            float y = motionEvent.getRawY();
+            final double tolerance = sqrt(pow(view.getWidth(), 2) + pow(view.getHeight(), 2)) / 10;
+
+            PuzzlePiece piece = (PuzzlePiece) view;
+            if (!piece.canMove) {
+                return true;
+            }
+
+            RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+            switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    xDelta = x - lParams.leftMargin;
+                    yDelta = y - lParams.topMargin;
+                    piece.bringToFront();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    lParams.leftMargin = (int) (x - xDelta);
+                    lParams.topMargin = (int) (y - yDelta);
+                    view.setLayoutParams(lParams);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    int xDiff = abs(piece.xCoord - lParams.leftMargin);
+                    int yDiff = abs(piece.yCoord - lParams.topMargin);
+                    if (xDiff <= tolerance && yDiff <= tolerance) {
+                        lParams.leftMargin = piece.xCoord;
+                        lParams.topMargin = piece.yCoord;
+                        piece.setLayoutParams(lParams);
+                        piece.canMove = false;
+                        sendViewToBack(piece);
+                    }
+                    break;
+            }
+
+            return true;
+        }
+
+        public void sendViewToBack(final View child) {
+            final ViewGroup parent = (ViewGroup)child.getParent();
+            if (null != parent) {
+                parent.removeView(child);
+                parent.addView(child, 0);
+            }
+        }
+    }
+
+    public class PuzzlePiece extends androidx.appcompat.widget.AppCompatImageView {
+        public int xCoord;
+        public int yCoord;
+        public int pieceWidth;
+        public int pieceHeight;
+        public boolean canMove = true;
+
+        public PuzzlePiece(Context context) {
+            super(context);
+        }
     }
 }
--- app/src/main/java/com/example/itcity/THEORY_TASK/security/ZadSec8.java
+++ app/src/main/java/com/example/itcity/THEORY_TASK/security/ZadSec8.java
@@ -1,26 +1,58 @@
 package com.example.itcity.THEORY_TASK.security;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.itcity.R;
+import com.example.itcity.THEORY_TASK.algorithm.Algorithm_HOME;
+import com.example.itcity.THEORY_TASK.algorithm.TheoryAlgorithm6;
+
+import java.util.ArrayList;
 
 public class ZadSec8 extends AppCompatActivity {
-    Button back;
-    Button next;
+    ArrayList<PuzzlePiece> pieces;
+    int res;
+    Button button5;
+    String strres;
+    Dialog dialog;
+    Button Check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sectask8);
         getSupportActionBar().hide();
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//выключение поворота экрана
-        back = (Button) findViewById(R.id.bottomSecurityK);
+        button5 = findViewById(R.id.bottomSecurityK);
+        Check = findViewById(R.id.continueSec);
+        final RelativeLayout layout = findViewById(R.id.Blayout);
+        pieces = splitImage();
+        TouchListener touchListener = new TouchListener();
+        for (PuzzlePiece piece : pieces) {
+            piece.setOnTouchListener(touchListener);
+            layout.addView(piece);
+        }
+
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,10 +61,137 @@ public class ZadSec8 extends AppCompatActivity {
                         Intent intent = new Intent(ZadSec8.this, TheorySecurity8.class);
                         startActivity(intent);
                         break;
-                }
+                    case R.id.continueSec:
+                        for (int i = 0; i < pieces.size(); i++) {
+                            if (pieces.get(i).canMove == false ) {
+                                res += 11;
+                            }
+                        }
+                        if (res == 99) {res += 1;}
+                        dialog = new Dialog(ZadSec8.this);
+                        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);//скрыть заголовок
+                        dialog.setContentView(R.layout.markgooddialogwindow);//путь к макету диалогового окна
+                        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//прозрачный фон
+                        dialog.setCancelable(false);//не закрывается кнопкой назад
+                        //кнопки начало
 
+                        //кнопки конец
+                        TextView result = dialog.findViewById(R.id.mark_for_the_lvl);
+                        strres = Integer.toString(res);
+                        result.setText(strres);
+                        dialog.show();//показ окна
+
+                        Button back_to_houses= dialog.findViewById(R.id.button10);
+                        back_to_houses.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(ZadSec8.this, Security_HOME.class);
+                                startActivity(intent);
+                            }
+                        });
+                        //ЕСЛИ БЫЛ ЗАПУЩЕН ЭТОТ БЛОК КОДА МЕНЯЕМ КОЛИЧЕСТВО БАЛЛОВ В FIREBASE  И В ПЕРЕМЕННУЮ В КОТОРОЙ НАШ УРОВЕНЬ ДЕЛАЕМ +1;
+                        break;
+                }
             }
         };
-        back.setOnClickListener(onClickListener);
+        button5.setOnClickListener(onClickListener);
+        Check.setOnClickListener(onClickListener);
+    }
+
+    private ArrayList<PuzzlePiece> splitImage() {
+        int piecesNumber = 9;
+        int rows = 3;
+        int cols = 3;
+
+        ImageView imageView = findViewById(R.id.imageView);
+        ArrayList<PuzzlePiece> pieces = new ArrayList<>(piecesNumber);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bezpuzzle6);
+
+        int pieceWidth = bitmap.getWidth()/cols;
+        int pieceHeight = bitmap.getHeight()/rows;
+
+        int yCoord = 0;
+        for (int row = 0; row < rows; row++) {
+            int xCoord = 0;
+            for (int col = 0; col < cols; col++) {
+                Bitmap pieceBitmap = Bitmap.createBitmap(bitmap, xCoord, yCoord, pieceWidth, pieceHeight);
+                PuzzlePiece piece = new PuzzlePiece(getApplicationContext());
+                piece.setImageBitmap(pieceBitmap);
+                piece.xCoord = xCoord;
+                piece.yCoord = yCoord;
+                piece.pieceWidth = pieceWidth;
+                piece.pieceHeight = pieceHeight;
+                pieces.add(piece);
+                xCoord += pieceWidth;
+            }
+            yCoord += pieceHeight;
+        }
+
+        return pieces;
+    }
+
+    public class TouchListener implements View.OnTouchListener {
+        private float xDelta;
+        private float yDelta;
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            float x = motionEvent.getRawX();
+            float y = motionEvent.getRawY();
+            final double tolerance = sqrt(pow(view.getWidth(), 2) + pow(view.getHeight(), 2)) / 10;
+
+            PuzzlePiece piece = (PuzzlePiece) view;
+            if (!piece.canMove) {
+                return true;
+            }
+
+            RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+            switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    xDelta = x - lParams.leftMargin;
+                    yDelta = y - lParams.topMargin;
+                    piece.bringToFront();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    lParams.leftMargin = (int) (x - xDelta);
+                    lParams.topMargin = (int) (y - yDelta);
+                    view.setLayoutParams(lParams);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    int xDiff = abs(piece.xCoord - lParams.leftMargin);
+                    int yDiff = abs(piece.yCoord - lParams.topMargin);
+                    if (xDiff <= tolerance && yDiff <= tolerance) {
+                        lParams.leftMargin = piece.xCoord;
+                        lParams.topMargin = piece.yCoord;
+                        piece.setLayoutParams(lParams);
+                        piece.canMove = false;
+                        sendViewToBack(piece);
+                    }
+                    break;
+            }
+
+            return true;
+        }
+
+        public void sendViewToBack(final View child) {
+            final ViewGroup parent = (ViewGroup)child.getParent();
+            if (null != parent) {
+                parent.removeView(child);
+                parent.addView(child, 0);
+            }
+        }
+    }
+
+    public class PuzzlePiece extends androidx.appcompat.widget.AppCompatImageView {
+        public int xCoord;
+        public int yCoord;
+        public int pieceWidth;
+        public int pieceHeight;
+        public boolean canMove = true;
+
+        public PuzzlePiece(Context context) {
+            super(context);
+        }
     }
 }
